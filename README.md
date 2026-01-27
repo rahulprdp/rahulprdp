fcc6e1b12dc64a7016c21c044fb08eae2f7e248e
Rahul Pradeep <rahulprdp@Rahuls-MacBook-Air.local>
2026-01-27 23:45:08 +0530

    feat: tailwind setup

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - 🌱 I’m currently working on Angular and TypeScript
 - 💞️ I’m looking to collaborate on Full stack Projects.
 - 📫 rahulprdp18@gmail.com
+- 👋 [Visit my website for more](https://rahulprdp.vercel.app)
 
 <!---
 rahulprdp/rahulprdp is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('rahulprdp');
+  public resumeUrl = "https://docs.google.com/document/d/13nWGr7eHm1HfJEnaAjvCSRfYHLATjBl45naOJnr5jmQ/edit?usp=sharing"
 }
